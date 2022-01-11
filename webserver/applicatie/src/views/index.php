03e62d7235ab19f91bc86e1680891622fd95f27c
MikeVerwegen <verwegenmi@gmail.com>
2022-01-11 19:55:48 +0100

    move

--- webserver/applicatie/src/views/index.php
+++ webserver/applicatie/src/views/index.php
@@ -2,6 +2,54 @@
 
 declare(strict_types=1);
 
-var_dump(PDO::getAvailableDrivers());
-
-phpinfo();
+?>
+<?php
+  $titel = "Home - Fletnix";
+  require_once 'components/head.php';
+  require_once 'components/header.php';
+  genereerHead($titel);
+?>
+  <body>
+    <div class="grid">
+      <header>
+        <?=
+        maakHeader();
+        ?>
+      </header>
+      <main>
+        <div class="preview-grid">
+          <spiderman>
+            <a href="mediaproduct.php">
+              <img src="images/thumbnail-spiderman.jpg" alt="Preview van Spider-Man: No Way Home"/>
+            </a>
+            <p>Spiderman: No Way Home</p>
+          </spiderman>
+          <bond>
+            <a href="mediaproduct.php">
+              <img src="images/thumbnail-jamesbond.jpg" alt="Preview van No Time To Die"/>
+            </a>
+            <p>No Time To Die</p>
+          </bond>
+          <venom>
+            <a href="mediaproduct.php">
+              <img src="images/thumbnail-venom.jpg" alt="Preview van Venom: Let There Be Carnage"/>
+            </a>
+            <p>Venom: Let There Be Carnage</p>
+          </venom>
+          <notice>
+            <a href="mediaproduct.php">
+              <img src="images/thumbnail-rednotice.jpg" alt="Preview van Red Notice"/>
+            </a>
+            <p>Red Notice</p>
+          </notice>
+          <grand>
+            <a href="mediaproduct.php">
+              <img src="images/thumbnail-grandtour.jpg" alt="Preview van The Grand Tour"/>
+            </a>
+            <p>The Grand Tour</p>
+          </grand>
+        </div>
+      </main>
+    </div>
+  </body>
+</html>
