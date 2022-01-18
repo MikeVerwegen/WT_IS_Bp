fa5f9523192cde72230afb013c89502034efa483
MikeVerwegen <verwegenmi@gmail.com>
2022-01-18 19:12:36 +0100

    header update

--- webserver/applicatie/src/views/components/header.php
+++ webserver/applicatie/src/views/components/header.php
@@ -5,9 +5,10 @@ function maakHeader() {
     <figure>
       <img src="images/logo.png" alt=""/>
     </figure>
-    <a href="bedrijf.php">Over ons</a>
-    <a href="privacy.php">Privacyverklaring</a>
     <a href="login.php">Inloggen</a>
+    <input type="text" placeholder="Zoeken">
+    <a href="privacy.php">Privacyverklaring</a>
+    <a href="bedrijf.php">Over ons</a>
   </div>
 HEAD;
   return $html;
